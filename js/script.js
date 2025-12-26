71e56b7164b97b1c1b995e589f81eb3dae7222ef
Vasyl Polyukhovich <69108694+vasil-polucohvich@users.noreply.github.com>
2025-12-26 16:47:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-let button = document.getElementById('button');
+/*let button = document.getElementById('button');
 let menu = document.getElementById('menu');
 button.addEventListener('click', function () {
     menu.classList.toggle('menu-show');
-})
+}) */
+const btn = document.getElementById('menuBtn');
+const menu = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+  const isOpen = btn.classList.toggle('is-open');
+
+  btn.setAttribute('aria-expanded', isOpen);
+  btn.setAttribute('aria-label', isOpen ? 'Закрити меню' : 'Відкрити меню');
+
+  menu.classList.toggle('open', isOpen);
+});
+
